ac52d5abf8e20a13fab62a48edc3074ec0cfd581
yutomasamura <yuto.masumura@favy.co.jp>
2019-08-25 16:36:08 +0900

    search_for_qitta_articles

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      items: null,
+      keyword: '',
+      message: ''
+    },
+    watch: {
+      keyword: function(newKeyword, oldKeyword) {
+        // console.log(newKeyword)
+        this.message = 'Waiting for you to stop typing'
+        this.debounceGetAnswer()
+      }
+    },
+    created: function() {
+      // this.keyword = 'Javascript'
+      // this.getAnswer()
+      this.debounceGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
+    methods: {
+      getAnswer: function() {
+        if(this.keyword === '') {
+          this.items = null
+          this.message = ''
+          return
+        }
+        this.message = 'Loading...'
+        var vm = this
+        var params = { page: 1, per_page: 20, query: this.keyword }
+        axios.get('https:qiita.com/api/v2/items', { params })
+          .then(function(response){
+            // console.log(response)
+            vm.items = response.data
+          })
+          .catch(function(error){
+            vm.message = 'Error!' + error
+          })
+          .finally(function(){
+            vm.message = ''
+          })
+      }
+    }
+})
